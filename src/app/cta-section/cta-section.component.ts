751c538bc67415e8e1dbfa4cca6b40618c410e67
TétéZer <arnaud.ndioazom@gmail.com>
2025-03-29 11:35:22 +0100

    wip create inits components

---
+++ src/app/cta-section/cta-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cta-section',
+  imports: [],
+  templateUrl: './cta-section.component.html',
+  standalone: true,
+  styleUrl: './cta-section.component.scss'
+})
+export class CtaSectionComponent {
+
+}
---
+++ src/app/feature-section/feature-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-section',
+  imports: [],
+  templateUrl: './feature-section.component.html',
+  standalone: true,
+  styleUrl: './feature-section.component.scss'
+})
+export class FeatureSectionComponent {
+
+}
